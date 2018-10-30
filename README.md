1af1b30162750b3ce7bd9bee5b611ea8e701288f
Tyler Mutch <tyler@servalldev.com>
2018-10-30 17:32:31 +0000

    Germinator first draft

---
+++ README.md
@@ -0,0 +1,90 @@
+# Postgres/Typeorm database seeder
+
+> Uses .yaml files to safely seed postgres databases
+
+## Why this?
+
+Typeorm doesn't have a common paradigm for running seeds, germinator aims to solve that problem in a simple way
+
+## How does it work?
+
+Germinator exports a single function that accepts a typeorm connection and a path to a directory containing both dev and prod seeds, it reads in the yaml files and seeds the appropriate entities with the data
+
+## Getting started
+
+1. Install in your project:
+
+    **NPM**
+    ```bash
+    npm install --save @servall/germinator
+    ```
+    **Yarn**
+    ```bash
+    yarn add @servall/germinator
+    ```
+
+2. Add a `seeds` directory to the root of your project
+
+3. Add a `dev` and `prod` directory under the `seeds` directory
+
+4. Add a new seeds file under `dev` let's call it `users.yaml`
+
+    ```yaml
+    entities:
+      - User:
+          firstName: 'Test'
+          lastName: 'User'
+    ```
+
+5. Import the package into your index.ts and call run seeds
+
+      ```javascript
+      import { runSeeds } from '@servall/germinator';
+
+      const connection = await connect(config.database);
+
+      await runSeeds(connection, '/path/to/seed/directory');
+      ```
+
+## Anatomy of the germinator yaml file
+
+- Every geminator yaml file starts with an `entities` key
+    ```yaml
+    entities:
+    ```
+
+- You can then list all the entities and their properties you'd like to insert
+    ```yaml
+    entities:
+      - User:
+          firstName: 'Test'
+          lastName: 'User'
+      - Department:
+          description: 'A test department'
+    ```
+
+- If you'd like to insert children of an entity you can use the special `refName` and `ref` keys on the entity
+
+    ```yaml
+    entities:
+      - User:
+          firstName: 'Test'
+          lastName: 'User'
+          refName: 'user-1'
+      - Department:
+          description: 'A test department'
+          user:
+            ref: 'user-1'
+    ```
+
+- If you'd like to insert sensitive data you can use the `sensitive` keys on the entities props, this will salt and hash the value before insertion
+
+    ```yaml
+    entities:
+      - User:
+          firstName: 'Test'
+          lastName: 'User'
+          password:
+            sensitive: true
+            value: 'test123'
+    ```
---
+++ src/index.ts
@@ -0,0 +1,232 @@
+import { Table, Connection, QueryRunner } from 'typeorm';
+import { extname, basename } from 'path';
+import { hashSync, genSaltSync } from 'bcrypt';
+import { readFile, readdir } from 'fs-extra';
+import { safeLoad } from 'js-yaml';
+import * as klaw from 'klaw';
+import { obj } from 'through2';
+
+export interface Seed {
+  name: string;
+  tables: TableSeed[];
+}
+
+export interface TableSeed {
+  name: string;
+  columns: string[];
+  values: string[];
+  sensitive: string[];
+}
+
+interface RawYaml {
+  name: string;
+  data: any;
+}
+
+export async function runSeeds(connection: Connection, seedLocation: string): Promise<number> {
+  const environment = process.env.NODE_ENV === 'development' ? 'dev' : 'prod';
+  const seedDirectory = `/${seedLocation}/${environment}`;
+  const refs: any = {};
+
+  const runner = connection.createQueryRunner();
+  let seedsLength = 0;
+
+  try {
+    await createSeedTableIfDoesNotExist(runner);
+
+    const doneSeeds = await getDoneSeeds(runner);
+
+    const files = await getYamlFilePaths(seedDirectory);
+
+    const seeds = await Promise.all(
+      files.map(loadYaml),
+    ) as any;
+
+    const filteredSeeds = seeds
+      .filter((seed:any) => !doneSeeds.includes(seed.name));
+
+    if (filteredSeeds.length > 0) {
+      await runner.startTransaction();
+
+      const promises = filteredSeeds.map(async (seed: any) => {
+        const { data: { entities } } = seed;
+
+        await entities.reduce(
+          async (promise: Promise<any>, entity: any) => {
+            await promise;
+            const entityName = Object.keys(entity)[0];
+            const data = entity[entityName];
+
+            const values = sortEntities(data).reduce(
+              (data: any, curr: [string, any]) => {
+                const [propName, prop] = curr;
+
+                let value = prop;
+
+                if (prop.sensitive) {
+                  const salt = genSaltSync();
+                  value = hashSync(prop.value, salt);
+                }
+
+                if (prop.ref) {
+                  if (!refs[prop.ref]) {
+                    throw new Error(
+                      'Cannot find reference, are the entities sorted correctly?'
+                    );
+                  }
+
+                  value = refs[prop.ref];
+                }
+
+                return {
+                  ...data,
+                  [propName]: value,
+                };
+              },
+              {},
+            );
+
+            const refName = data.refName;
+
+            const saved = await runner.connection
+              .createQueryBuilder()
+              .insert()
+              .into(entityName)
+              .values(values)
+              .execute();
+
+            if (refName) {
+              refs[refName] = saved.identifiers[0];
+            }
+
+            return Promise.resolve();
+          },
+          Promise.resolve(),
+        );
+
+        return insertSeed(seed.name, runner);
+      });
+
+      await Promise.all(promises);
+    }
+
+    seedsLength = filteredSeeds.length;
+
+    if (runner.isTransactionActive) {
+      await runner.commitTransaction();
+    }
+  } catch (e) {
+    console.error(e);
+    if (runner.isTransactionActive) {
+      await runner.rollbackTransaction();
+    }
+  } finally {
+    await runner.release();
+  }
+
+  return seedsLength;
+}
+
+export async function createSeedTableIfDoesNotExist(runner: QueryRunner): Promise<void> {
+  await runner.createTable(
+    new Table({
+      name: 'seeds',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          isGenerated: true,
+        }, {
+          name: 'timestamp',
+          type: 'timestamptz',
+          isNullable: false,
+        }, {
+          name: 'name',
+          type: 'text',
+          isNullable: false,
+        },
+      ],
+    }),
+    true,
+  );
+}
+
+export async function insertSeed(name: string, runner: QueryRunner): Promise<void> {
+  await runner.query(
+    'INSERT INTO "seeds" ("timestamp", "name") VALUES(CURRENT_TIMESTAMP, ?)',
+    [name],
+  );
+}
+
+export async function getDoneSeeds(runner: QueryRunner): Promise<string[]> {
+  const seeds = await runner.query('SELECT * FROM seeds');
+  return seeds.map((seed: any) => seed.name);
+}
+
+export function getYamlFilePaths(seedsPath: string): Promise<string[]> {
+  const excludeDirFilter = obj(function (item, enc, next) {
+    if (!item.stats.isDirectory() && isYamlFile(item.path)) {
+      this.push(item);
+    }
+    next();
+  });
+
+  const files: string[] = [];
+  return new Promise(
+    (resolve: Function, reject: Function) => {
+      const klawer = klaw(
+        seedsPath,
+        {
+          depthLimit: 1,
+        },
+      );
+
+      klawer.on('error', reject);
+      klawer
+        .pipe(excludeDirFilter)
+        .on('data', file => files.push(file.path));
+      klawer.on('end', () => resolve(files));
+    },
+  );
+}
+
+export async function loadYaml(filePath: string): Promise<RawYaml> {
+  const file = await readFile(filePath);
+
+  return {
+    name: basename(filePath, '.yaml'),
+    data: safeLoad(file.toString()),
+  };
+}
+
+export function isYamlFile(item: any): boolean {
+  return extname(item).match(/\.ya?ml$/) !== null;
+}
+
+export function sortEntities(entities: any): [string, any][] {
+  const arr = Object.entries(entities);
+  const newArr: any[] = [];
+
+  arr.forEach((item: [string, any]) => {
+    const [_, props] = item;
+    let smallestIdx = Infinity;
+
+    newArr.forEach((otherItem: [string, any], index: number) => {
+      const refs = Object.values(otherItem[1]).filter((i: any) => i.ref !== undefined);
+      const idx = refs.indexOf(props.refName);
+
+      if (idx >= 0 && index < smallestIdx) {
+        smallestIdx = index;
+      }
+    });
+
+    if (smallestIdx === Infinity) {
+      newArr.push(item);
+    } else {
+      newArr.splice(smallestIdx, 0, item);
+    }
+  });
+
+  return newArr;
+}
